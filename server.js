7c180deb6aaab091ea12ceb6a3677ad19a377c41
Jessica Lee <leej28@oregonstate.edu>
2025-11-24 16:39:34 -0800

    Basic EJS and highscores.json.

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require("express")
+var highscores = require("./highscores.json")
+
 var app = express()
-app.use(express.static("static/"))
+var port = process.env.PORT || 8000
+
+app.set("view engine", "ejs")
+app.use(express.static("static"))
 
 app.get("/", function (req, res, next) {
-    res.status(200).sendFile(__dirname + "/static/game.html")
+    res.status(200).render("game", {
+        scores: highscores
+    })
 })
 
 app.get("*splat", function (req, res, next) {
-    res.status(404).sendFile(__dirname + "/static/404.html")
+    res.status(404).render("404", {
+        page: req.url
+    })
 })
 
-app.listen(8000, function () {
-    console.log("== Server is listening on port 8000.")
+app.listen(port, function () {
+    console.log("== Server is listening on port " + port + ".")
 })
