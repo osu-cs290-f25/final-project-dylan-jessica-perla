c0ac9eb37edc3b57db8d999308b034f7bef1e0b9
Perla CruzMendoza <cruzmenp@flip1.engr.oregonstate.edu>
2025-12-05 21:48:36 -0800

    Adding all files

--- static/game.js
+++ static/game.js
@@ -1,1 +1,12 @@
 console.log("Test game.js.")
+var actionButtons = document.querySelectorAll(".acction-button");
+var scoreDisplay = document.querySelectorAll(".score-container");
+var modal = document.getElementsById(".game-over-modal");
+var closeButton = document.querySelector(".close-button");
+var playerName = document.getElementById(".player-name");
+
+let sequence = [];
+let playerSequence = [];
+let playerScore = 0;
+let playing = false;
+
