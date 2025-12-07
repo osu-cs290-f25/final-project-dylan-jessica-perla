37cc5c28044c072b99198c4f9d48e1da9ad97b96
Perla CruzMendoza <cruzmenp@flip2.engr.oregonstate.edu>
2025-12-06 18:48:16 -0800

    implemented the flashing sequence aspect of the game (also takes user's click sequence

--- static/game.js
+++ static/game.js
@@ -1,12 +1,89 @@
 console.log("Test game.js.")
-var actionButtons = document.querySelectorAll(".acction-button");
-var scoreDisplay = document.querySelectorAll(".score-container");
-var modal = document.getElementsById(".game-over-modal");
+var actionButtons = document.querySelectorAll(".action-button");
+var scoreDisplay = document.querySelector(".score-container");
+var modal = document.getElementById("game-over-modal");
 var closeButton = document.querySelector(".close-button");
-var playerName = document.getElementById(".player-name");
+var playerName = document.getElementById("player-name");
+var finalScore = document.querySelector("p");
+
 
 let sequence = [];
 let playerSequence = [];
 let playerScore = 0;
 let playing = false;
+let playerHighscores = [];
 
+
+function gameStart(){
+    sequence = [];
+    playerSequence = [];
+    playerScore = 0;
+    playing = true;
+    updateScore();
+    round();
+
+}
+
+function round(){
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    sequence.push(randomColor);
+    playerSequence = [];
+
+    let delay = 0;
+
+    sequence.forEach(color=>{
+        setTimeout(()=>{
+            var button = document.getElementById(color);
+            button.style.filter = "brightness(2)";
+            button.style.boxShadow = "0 0 15px white";
+            setTimeout(()=>{
+                button.style.filter = "";
+                button.style.boxShadow = "";
+            }, 800);
+        }, delay);
+        delay += 500;
+    });
+}
+
+function playerInput(color){
+    if (!playing){
+        return;
+    }
+
+    playerSequence.push(color);
+    var index = playerSequence.length - 1;
+
+    if(playerSequence[index] != sequence[index]){
+        gameOver();
+        return;
+    }
+
+    if(playerSequence.length == sequence.length){
+        playerScore++;
+        updateScore();
+        setTimeout(round, 1000);
+    }
+}
+
+function updateScore(){
+    scoreDisplay.textContent = 'Score: ${playerScore} pts';
+}
+
+function gameOver(){
+    playing = false;
+    modal.style.display = "block";
+    
+    finalScore.textContent = 'Your final score is: ${playerScore} pts';
+
+}
+
+actionButtons.forEach(button =>{
+    button.addEventListener("click", () => playerInput(button.id));
+});
+
+closeButton.addEventListener("click", ()=>{
+    modal.style.display = "none";
+});
+
+gameStart();
